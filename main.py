1c236926f0229c1f1dbfdd9c72975f434a748acf
tero51 <akmalmagistr@gmail.com>
2024-11-11 04:39:25 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# def send_email(message, recipient,*, sender="university.help@gmail.com"):
+#     if "@" not in recipient or not (recipient.endswith(".com") or recipient.endswith(".ru") or recipient.endswith(".net")):
+#         print("Невозможно отправить письмо с адреса " + sender + " на адрес " + recipient)
+#     elif sender == recipient:
+#         print("Нельзя отправить письмо самому себе!")
+#     elif sender == "university.help@gmail.com":
+#         print("Письмо успешно отправлено с адреса " + sender + " на адрес " + recipient)
+#     else:
+#         print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса " + sender + " на адрес " + recipient)
+#
+#
+#
+# send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+#
+# send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+#
+# send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+#
+# send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+# первый варик, оставлю что бы был.
+
+
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    def is_valid_email(email):
+        return "@" in email and (email.endswith(".com") or email.endswith(".ru") or email.endswith(".net"))
+
+
+    if not is_valid_email(sender) or not is_valid_email(recipient):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
+    return message # что бы он хоть как-то учавствовал, а то нигде не вызывается фактически.
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
